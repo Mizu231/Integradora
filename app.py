1762aea54b78eba2ce0db0c35303478a9a125f4f
Esther Her <lupitaherrera8491@gmail.com>
2026-02-16 19:56:35 -0600

    Solucionesí

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def bienvenida():
     return render_template('bienvenida.html')
 
-@app.route('/inicio')
+@app.route('/inicio', methods=['GET'])
 def inicio():
     return render_template('inicio.html')
 
-@app.route('/panel')
+@app.route('/panel', methods=['GET'])
 def panel():
     return render_template('panel.html')
 
-@app.route('/historial')
+@app.route('/historial', methods=['GET'])
 def historial():
     return render_template('historial.html')
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
     return render_template('admin.html')
 
